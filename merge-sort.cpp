dcc5248b66b4a5cd6359d9bcc13a11690f5ce6c1
Evan Stark <estark3@charlotte.edu>
2025-02-05 18:38:31 -0500

    Finalizing merge-sort.cpp

--- merge-sort.cpp
+++ merge-sort.cpp
@@ -1,21 +1,12 @@
 // Evan Stark - January 24th 2025 - ITCS 4145 001
 // This C++ program simulates a basic Merge Sort Algorithm.
-// It accomplishes this by recursively dividing the array in half until no longer
-// and then rebuilds the arrays from least to greatest.
-// 
-// We will need to keep track of the lowest, middle, and highest index numbers to
-// ensure the array is correctly halved.
 
-// Needed imports.
 #include <iostream>
 #include <cstdlib>
-#include <vector>
-#include <chrono>
 
 // SOURCES USED
 // https://www.geeksforgeeks.org/rand-and-srand-in-ccpp/?ref=header_outind
 // https://www.geeksforgeeks.org/how-to-generate-random-number-in-range-in-cpp/
-// https://www.geeksforgeeks.org/why-rand-function-always-give-the-same-value/
 // https://www.youtube.com/watch?v=qB4ZQv-IVkk
 // https://www.geeksforgeeks.org/cin-in-c/?ref=header_outind
 // https://www.youtube.com/watch?v=4VqmGXwpLqc
@@ -23,103 +14,154 @@
 // https://www.tutorialspoint.com/cplusplus/cpp_return_arrays_from_functions.htm
 // https://stackoverflow.com/questions/10290610/how-can-i-find-the-number-of-elements-in-an-array
 // https://www.geeksforgeeks.org/cpp-pointers/?ref=gcse_outind
-// https://takeuforward.org/data-structure/merge-sort-algorithm/
-// https://stackoverflow.com/questions/22387586/measuring-execution-time-of-a-function-in-c
-// https://godbolt.org/z/oe5cMd
-// https://devdocs.io/cpp/header/chrono
 
 // A brief ChatGPT discussion was held to attempt to debug segmentation faults.
 // When this conversation occurred, I had spent roughly 1-1.5 hours on debugging this issue.
 // https://chatgpt.com/share/679aad60-3bd8-8001-831d-e00a8827ead3
 
-// ADDITIONAL CODE ASSISTANCE PROVIDED BY TA DAVASHISH BHAT DURING OFFICE HOURS //
-
 using namespace std;
 
-void merge(int* arr, int low, int mid, int high) {
-    // Initialize a temporary array.
-    // Vector is used here to take advantage of its array operations.
-    vector<int> temp_array;
+int* merge(int* arr_1, int* arr_2) {
+    // Calculating the final size of the sorted array.
+    int arr_1_size = (sizeof(arr_1) / sizeof(int));
+    int arr_2_size = (sizeof(arr_2) / sizeof(int));
+    int final_size = arr_1_size + arr_2_size;
+    // Initialize the sorted array.
+    int* sorted_array = new int[final_size];
+    
+    // 
+    int rebuild_arr_1_index = 0;
+    int rebuild_arr_2_index = 0;
+    int final_size_index = final_size - 1;
 
-    // Get starting points of the left and right sub-arrays.
-    int left = low;
-    int right = mid+1;
-
-    while(left <= mid && right <= high) {
-        // If left size element <= to right side element --> put into array.
-        if(arr[left] <= arr[right]) {
-            temp_array.push_back(arr[left]);
-            left++;
+    while(rebuild_arr_1_index < arr_1_size && rebuild_arr_2_index < arr_2_size) {
+        // If the
+        if(arr_1[0] > arr_2[0]) {
+            sorted_array[final_size_index] = arr_2[0];
+            final_size_index--;
+            // Rebuilding arr_2.
+            for(int a=rebuild_arr_2_index; a < arr_2_size; a++) {
+                arr_2[0] = arr_2[a];
+            }
+            // Increment the index to make next array smaller.
+            rebuild_arr_2_index++;
         }
-        // Otherwise, put right side element in the array.
+
         else {
-            temp_array.push_back(arr[right]);
-            right++;
+            sorted_array[final_size_index] = arr_1[0];
+            final_size_index--;
+            // Rebuilding arr_1.
+            for(int a=rebuild_arr_1_index; a<arr_2_size; a++) {
+                arr_1[0] = arr_1[a];
+            }
+            // Increment index to make next array smaller.
+            rebuild_arr_1_index++;
         }
     
     }
 
-    // Put remaining left side elements in the sorted array.
-    while(left <= mid) {
-        temp_array.push_back(arr[left]);
-        left++;
-    }
-    // Remaining right side elements put in the sorted array.
-    while(right <= high) {
-        temp_array.push_back(arr[right]);
-        right++;
-    }
-    
-    // Rebuilding the originial array.
-    // Elements added in reverse.
-    for (int a=high; a >= low; a--) {
-        arr[a] = temp_array.back();
-        temp_array.pop_back();
-    }
+    // Put remaining arr_1 elements in the sorted array.
+    while(rebuild_arr_1_index < arr_1_size) {
+        sorted_array[final_size_index] = arr_1[0];
+        final_size_index--;
+        // Same rebuilding logic as above.
+        for(int a=rebuild_arr_1_index; a<arr_1_size; a++) {
+            arr_1[0] = arr_1[a];
+        }
 
+    }
+    // Remaining arr_2 elements put in the sorted array.
+    while(rebuild_arr_2_index < arr_2_size) {
+        sorted_array[final_size-1] = arr_2[0];
+        // Same rebuilding logic as above.
+        for(int a=rebuild_arr_2_index; a<arr_2_size; a++) {
+            arr_2[0] = arr_2[a];
+        }
+        rebuild_arr_2_index++;
+    }
+    return sorted_array;
 }
 
 
 
-void merge_sort(int* arr, int low, int high) {
-    // Return if array has one element; i.e. low's index is greater/equal to than the high index.
-    if (low >= high) {
-        return;
+int* merge_sort(int* arr) {
+    // Getting the size of the array.
+    int size = sizeof(arr) / sizeof(int);
+    // Pointer for the init_array to allocate space.
+    int* init_array = new int[size];
+    
+    // Return if array has one element.
+    if (size/2 == 1) {
+        return init_array;
+    }
+
+    // Getting the size / 2 for splitting the array.
+    int half_size = size / 2;
+    int sec_half_size = 0;
+    // Handling odd numbered amount of elements.
+    if (size % 2 == 1) {
+        sec_half_size = (size/2) + 1;
+    }
+    else {
+        sec_half_size = size / 2;
     }
     
-    // Calculate the middle index.
-    int mid = (low + high) / 2;
+    // Initialize arrays.
+    int* arr_1 = new int[half_size];
+    int* arr_2 = new int [sec_half_size];
+
+    // Populating the arrays.
+    for(int a=0; a < half_size; a++){
+        arr_1[a] = init_array[a];
+    }
+    for(int a=sec_half_size + 1; a < size; a++){
+        arr_2[a] = init_array[a];
+    }
+
+    cout << "1st-half: " << half_size << "\n";      // DEBUG
+    cout << "2nd half: " << sec_half_size <<"\n";   // DEBUG
         
-    // Recursive call on the left half.
-    merge_sort(arr,low, mid);
-    // Recursive call on the right half.
-    merge_sort(arr, mid+1, high);
-    // Merge the halves together.
-    merge(arr, low, mid, high);
+    // Run a recursive call on both functions.
+    arr_1 = merge_sort(arr_1);
+    arr_2 = merge_sort(arr_2);
+
+    // Call the merge function.
+    int* merge_array = merge(arr_1, arr_2);
+    return merge_array;
 }
 
-
-
-// Function to generate data
 void generate_data(int* arr, size_t n)
 {
-    // Call srand to ensure different output.
-    srand(time(0));
-    
     for(int i=0;i<n;i++)
     {
-        // Cap elements from the integers 0-49.
-        arr[i] = rand() % 50;
+        arr[i]=rand();
     }
 }
 
 
 
 // Main function for running the program.
-void initialize(int init_size) {
-    // Create the initial array.
+int main() {
+    int init_size;
+    std::cout << "How many elements would you like to sort? \n";
+    std:cin >> init_size;
+    // Cap number of elements to 50.
+    if(init_size > 50) {
+        init_size = 50;
+    }
+    // Minimum of 4 elements.
+    if (init_size < 4) {
+        init_size = 4;
+    }
+
+    //Initialize the array.
     int* init_array = new int[init_size];
-    generate_data(init_array, init_size);
+    for (int a=0; a<init_size; a++) {
+        // Get a number from 0 thru 49.
+        int rng_element = rand() % 50;
+        init_array[a] = rng_element;
+    }
+
 
     // Print out initial array.
     std::cout << "Initial array: ";
@@ -127,35 +169,14 @@ void initialize(int init_size) {
         std::cout << init_array[a] << " ";
     }
     
-    // Get the low and high indexes, and call merge_sort function.
-    int low = init_size - init_size;
-    int high = init_size - 1;
-
-    // Calculating run-time.
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration;
-    using std::chrono::duration_cast;
-    using std::chrono::milliseconds;
+    // Pointer for the output/sorted array and print the output.
+    int* output = new int [init_size];
+    output = merge_sort(init_array);
     
-    auto start_time = high_resolution_clock::now();
-    merge_sort(init_array, low, high);
-    auto end_time = high_resolution_clock::now();
-    duration<double, std::milli> merge_duration = end_time - start_time;
-
-    // Print sorted array.
-    std::cout << "\nSorted array: ";
+    std::cout << "Sorted array: ";
     for (int a=0; a < init_size; a++) {
-        std::cout << init_array[a] << " ";
+        std::cout << output[a] << " ";
     }
-    std::cout << "\n";
 
-    std::cout << "\nTime taken = " << merge_duration.count() << " milliseconds\n";
-
-    // Deallocate the array pointer.
-    delete[] init_array;
-}
-
-// Main function
-int main() {
     return 0;
 }
