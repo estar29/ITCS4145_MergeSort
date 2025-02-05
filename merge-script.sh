9029de18b8771135e9ef80149e98f9c188ba51df
Evan Stark <estark3@charlotte.edu>
2025-02-04 22:08:31 -0500

    Modified merge-script

--- merge-script.sh
+++ merge-script.sh
@@ -4,3 +4,13 @@
 #SBATCH --partition=Centaurus
 #SBATCH --time=02:00:00
 #SBATCH --mem=2G
+
+./merge-sort 10
+./merge-sort 100
+./merge-sort 1000
+./merge-sort 10000
+./merge-sort 100000
+./merge-sort 1000000
+./merge-sort 10000000
+./merge-sort 100000000
+./merge-sort 1000000000
